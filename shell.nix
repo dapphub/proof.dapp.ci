7a5b45be739bbe6267b0b1119920e90106988a19
Denis <denis.erfurt@gmail.com>
2019-03-27 23:08:38 +0100

    update klab and add shell.nix

---
+++ shell.nix
@@ -0,0 +1,27 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "klab-env";
+  buildInputs = [
+    autoconf
+    flex
+    gcc
+    git
+    gmp
+    gnumake
+    maven
+    mpfr
+    ncurses
+    nodejs
+    opam
+    openjdk8
+    pandoc
+    pkgconfig
+    python
+    python3
+    z3
+  ];
+  shellHook = ''
+    export PATH=$PWD/node_modules/.bin/:$PWD/bin:$PATH
+    export KLAB_EVMS_PATH="''${KLAB_EVMS_PATH:-''${PWD}/evm-semantics}"
+  '';
+}
